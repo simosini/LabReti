023f7656da2f7296c1f12699b4ef55517205ec6e
Massimiliano Poggi <massimiliano.poggi@gmail.com>
2015-12-01 11:35:18 +0100

    Started Socket lab 4

--- Socket/Lab04/ServerIterativo.java
+++ Socket/Lab04/ServerIterativo.java
@@ -1,7 +1,6 @@
 import java.net.InetAddress;
 import java.net.Socket;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
--- Socket/Lab04/ServerMultithread.java
+++ Socket/Lab04/ServerMultithread.java
@@ -2,10 +2,6 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 public class ServerMultithread
 {
 	public static void main(String[] args) 
